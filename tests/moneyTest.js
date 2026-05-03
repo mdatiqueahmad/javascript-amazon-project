8130a927de624c85988ab336f5a7e441171efe4b
mdatiqueahmad <mdatiqueahmad16@gmail.com>
2026-05-03 21:47:28 +0530

    Rename tes-jasmine folder to tests

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,29 +1,16 @@
 import{formatCurrency} from '../scripts/utils/money.js';
 
-console.log('test suite :formatCurrency')
+describe('test suite: formatCurrency' , () =>
+{
+  it('converts cents into dollars' , () =>{
+    expect(formatCurrency(2095)).toEqual('20.95');
+  });
 
-console.log('converts cents into dollars')
-if (formatCurrency(2095) ==='20.95') {
-console.log('passed');
-} else {
-  console.log('failed');
-}
+   it('works with 0' , () =>{
+    expect(formatCurrency(0)).toEqual('0.00');
+  });
 
-console.log('works with 0')
-if(formatCurrency(0) ==='0.00') {
-  console.log('passed');
-} else {
-  console.log('failed');
-}
-
-if(formatCurrency(2000.4) ==='20.00') {
-  console.log('passed');
-} else {
-  console.log('failed');
-}
-console.log('rounds up to the nearest cent')
-if(formatCurrency(2000.5) ==='20.01') {
-  console.log('passed');
-} else {
-  console.log('failed');
-}
+   it('rounds up to the nearest cent' , () =>{
+    expect(formatCurrency(2000.5)).toEqual('20.01');
+  });
+});
